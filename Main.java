e097d63da3776df4c6b3e10d0a58567f4b836505
Maria Garlińska <162016875+xMvasy@users.noreply.github.com>
2024-05-05 11:51:09 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main {
         Worker worker3 = new Worker("Josh", 3200.00, 3, "2023-01-20", "Operator");
         Worker worker4 = new Worker("Louise", 3300.00, 4, "2021-12-01", "Cleaner");
 
-        Manager manager = new Manager("Adam", 5000.00, 5, "2019-07-30", "Manager");
+        Manager manager = new Manager("Adam", 5000.00, 201, "2019-07-30", "Manager");
         Manager manager1 = new Manager("Michael", 5000, 201, "2021-11-01", "Project Manager");
         Manager manager2 = new Manager("Anna", 5500, 202, "2021-10-15", "Senior Manager");
         Manager manager3 = new Manager("Peter", 6000, 203, "2022-01-05", "Director");
